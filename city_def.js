1abee05efb78cfe46bd5b18cf955ecfd4fb0fc15
cz425 <marquardt.clara@gmail.com>
2018-08-11 21:10:56 +0200

    General Update 08/08/2018

---
+++ city_def.js
@@ -0,0 +1,435 @@
+// Parameter Def
+online=false
+// online=true
+
+zoom_min=10
+zoom_max=19
+
+// Variable Def
+
+zoom = Math.floor(Math.random() * (zoom_max - zoom_min + 1)) + zoom_min;
+
+// * city_1
+var city_1="Osnabrueck"
+var city_1_vis = "vis/index_city_1.html"
+var city_1_label_1 = "Osnabrueck, Germany"
+var city_1_label_2 = "| 1 Year 2 Months"
+var city_1_label_3 = "|| Growing Up"
+var city_1_coordinate_1 = 52.2799
+var city_1_coordinate_2 = 8.0472
+
+if (online==true) {
+   var city_1_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_1+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_1_url = "image/image_1.png"
+}
+
+// * city_2
+var city_2="Wiesbaden"
+var city_2_vis = "vis/index_city_2.html"
+var city_2_label_1 = "Wiesbaden, Germany"
+var city_2_label_2 = "| 1 Year 3 Months"
+var city_2_label_3 = "|| Growing Up"
+var city_2_coordinate_1 = 50.0782
+var city_2_coordinate_2 = 8.2398
+
+if (online==true) {
+   var city_2_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_2+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_2_url = "image/image_2.png"
+}
+
+// * city_3
+var city_3="Kassel"
+var city_3_vis = "vis/index_city_3.html"
+var city_3_label_1 = "Kassel, Germany"
+var city_3_label_2 = "| 2 Years 3 Months"
+var city_3_label_3 = "|| Growing Up"
+var city_3_coordinate_1 = 51.2848
+var city_3_coordinate_2 = 9.6203
+
+if (online==true) {
+   var city_3_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_3+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_3_url = "image/image_3.png"
+}
+
+// * city_4
+var city_4="Karlsruhe,Waldstadt"
+var city_4_vis = "vis/index_city_4.html"
+var city_4_label_1 = "Karlsruhe, Germany"
+var city_4_label_2 = "| 1 Year 8 Months"
+var city_4_label_3 = "|| Attending Kindergarten"
+var city_4_coordinate_1 = 49.0424
+var city_4_coordinate_2 = 8.4268
+
+if (online==true) {
+   var city_4_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_4+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_4_url = "image/image_4.png"
+}
+
+// * city_5
+var city_5="Karlsruhe"
+var city_5_vis = "vis/index_city_5.html"
+var city_5_label_1 = "Karlsruhe, Germany"
+var city_5_label_2 = "| 4 Years 1 Month"
+var city_5_label_3 = "|| Attending Primary School"
+var city_5_coordinate_1 = 49.0069
+var city_5_coordinate_2 = 8.4037
+
+if (online==true) {
+   var city_5_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_5+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_5_url = "image/image_5.png"
+}
+
+// * city_6
+var city_6="Copenhagen"
+var city_6_vis = "vis/index_city_6.html"
+var city_6_label_1 = "København (Copenhagen), Denmark"
+var city_6_label_2 = "| 1 Year"
+var city_6_label_3 = "|| Attending Middle School"
+var city_6_coordinate_1 = 55.6761
+var city_6_coordinate_2 = 12.5683
+
+if (online==true) {
+   var city_6_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_6+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_6_url = "image/image_6.png"
+}
+
+// * city_7
+var city_7="Karlsruhe"
+var city_7_vis = "vis/index_city_7.html"
+var city_7_label_1 = "Karlsruhe, Germany"
+var city_7_label_2 = "| 1 Year"
+var city_7_label_3 = "|| Attending Middle School"
+var city_7_coordinate_1 = 49.0069
+var city_7_coordinate_2 = 8.4037
+
+if (online==true) {
+   var city_7_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_7+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_7_url = "image/image_7.png"
+}
+
+// * city_8
+var city_8="Frankfurt"
+var city_8_vis = "vis/index_city_8.html"
+var city_8_label_1 = "Frankfurt, Germany"
+var city_8_label_2 = "| 1 Year 9 Months"
+var city_8_label_3 = "|| Attending Middle School"
+var city_8_coordinate_1 = 50.1109
+var city_8_coordinate_2 = 8.6821
+
+if (online==true) {
+   var city_8_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_8+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_8_url = "image/image_8.png"
+}
+
+// * city_9
+var city_9="Kaifaqu"
+var city_9_vis = "vis/index_city_8.html"
+var city_9_label_1 = "开发区 (Kaifaqu), China"
+var city_9_label_2 = "| 1 Year 3 Months"
+var city_9_label_3 = "|| Attending High School"
+var city_9_coordinate_1 = 39.0536
+var city_9_coordinate_2 = 121.7746
+
+if (online==true) {
+   var city_9_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_9+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_9_url = "image/image_9.png"
+}
+
+// * city_10
+var city_10="Beijing"
+var city_10_vis = "vis/index_city_10.html"
+var city_10_label_1 = "北京 (Beijing), China"
+var city_10_label_2 = "| 1 Year 9 Months"
+var city_10_label_3 = "|| Graduating From School"
+var city_10_coordinate_1 = 39.9042
+var city_10_coordinate_2 = 116.4074
+
+if (online==true) {
+   var city_10_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_10+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_10_url = "image/image_10.png"
+}
+
+// * city_11
+var city_11="Dalian"
+var city_11_vis = "vis/index_city_11.html"
+var city_11_label_1 = "大连 (Dalian), China"
+var city_11_label_2 = "| 7 Months"
+var city_11_label_3 = "|| Working"
+var city_11_coordinate_1 = 38.914
+var city_11_coordinate_2 = 121.6147
+
+if (online==true) {
+   var city_11_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_11+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_11_url = "image/image_11.png"
+}
+
+// * city_11
+var city_12="Jiashan,Gansu"
+var city_12_vis = "vis/index_city_12.html"
+var city_12_label_1 = "架山 （Jiashan）, Germany"
+var city_12_label_2 = "| 3 Months"
+var city_12_label_3 = "|| Working"
+var city_12_coordinate_1 = 33.7688
+var city_12_coordinate_2 = 106.0878
+
+if (online==true) {
+   var city_12_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_12+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_12_url = "image/image_12.png"
+}
+
+// * city_13
+var city_13="Beijing"
+var city_13_vis = "vis/index_city_13.html"
+var city_13_label_1 = "北京 (Beijing), China"
+var city_13_label_2 = "| 5 Months"
+var city_13_label_3 = "|| Working"
+var city_13_coordinate_1 = 39.9042
+var city_13_coordinate_2 = 116.4074
+
+if (online==true) {
+   var city_13_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_13+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_13_url = "image/image_13.png"
+}
+
+// * city_14
+var city_14="Cambridge"
+var city_14_vis = "vis/index_city_14.html"
+var city_14_label_1 = "Cambridge, UK"
+var city_14_label_2 = "| 2 Years 5 Months"
+var city_14_label_3 = "|| Attending University (BA)"
+var city_14_coordinate_1 = 52.2053
+var city_14_coordinate_2 = 0.1218
+
+if (online==true) {
+   var city_14_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_14+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_14_url = "image/image_14.png"
+}
+
+// * city_15
+var city_15="London"
+var city_15_vis = "vis/index_city_15.html"
+var city_15_label_1 = "London, UK"
+var city_15_label_2 = "| 3 Months"
+var city_15_label_3 = "|| Working"
+var city_15_coordinate_1 = 51.5074
+var city_15_coordinate_2 = 0.1278
+
+if (online==true) {
+   var city_15_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_15+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_15_url = "image/image_15.png"
+}
+
+// * city_16
+var city_16="PhnomPenh"
+var city_16_vis = "vis/index_city_16.html"
+var city_16_label_1 = "Phnom Penh, Cambodia"
+var city_16_label_2 = "| 3 Months"
+var city_16_label_3 = "|| Working"
+var city_16_coordinate_1 = 1.5564
+var city_16_coordinate_2 = 104.9282
+
+if (online==true) {
+   var city_16_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_16+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_16_url = "image/image_16.png"
+}
+
+// * city_17
+var city_17="Cambridge,MA"
+var city_17_vis = "vis/index_city_17.html"
+var city_17_label_1 = "Cambridge, US"
+var city_17_label_2 = "| 8 Months"
+var city_17_label_3 = "|| Attending University (Post-Grad)"
+var city_17_coordinate_1 = 42.3736
+var city_17_coordinate_2 = 71.1097
+
+if (online==true) {
+   var city_17_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_17+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_17_url = "image/image_17.png"
+}
+
+// * city_18
+var city_18="SanFrancisco"
+var city_18_vis = "vis/index_city_18.html"
+var city_18_label_1 = "San Francisco, US"
+var city_18_label_2 = "| 6 Months"
+var city_18_label_3 = "|| Founding A Company"
+var city_18_coordinate_1 = 37.7749
+var city_18_coordinate_2 = 122.4194
+
+if (online==true) {
+   var city_18_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_18+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_18_url = "image/image_18.png"
+}
+
+// * city_19
+var city_19="Somerville"
+var city_19_vis = "vis/index_city_19.html"
+var city_19_label_1 = "Somerville, US"
+var city_19_label_2 = "| 1 Year 5 Months"
+var city_19_label_3 = "|| Working"
+var city_19_coordinate_1 = 42.3876
+var city_19_coordinate_2 = 71.0995
+
+if (online==true) {
+   var city_19_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_19+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_19_url = "image/image_19.png"
+}
+
+// * city_20
+var city_20="Chicago"
+var city_20_vis = "vis/index_city_20.html"
+var city_20_label_1 = "Chicago, US"
+var city_20_label_2 = "| 1 Year 5 Months"
+var city_20_label_3 = "|| Working"
+var city_20_coordinate_1 = 41.8781
+var city_20_coordinate_2 = 87.6298
+
+if (online==true) {
+   var city_20_url = "https://maps.googleapis.com/maps/api/staticmap?center="+city_20+"&"+zoom+"&size=600x300&maptype=satellite&key=ABQIAAAATfHumDbW3OmRByfquHd3SRTRERdeAiwZ9EeJWta3L_JZVS0bOBRQeZgr4K0xyVKzUdnnuFl8X9PX0w"
+} else {
+   var city_20_url = "image/image_20.png"
+}
+
+// Function Def
+function draw_globe(globe_data, city_label_1,city_label_2,city_label_3, city_coordinate_1, city_coordinate_2) {
+
+// Map configuration
+var width  = 820;
+var height = 620;
+var rScale = d3.scale.sqrt();
+var peoplePerPixel = 1000000;
+var max_population = [];
+
+// Configuration for the spinning effect
+var time = Date.now();
+var rotate = [0, 0];
+var velocity = [.015, -0];
+
+// set projection type and paremetes
+var projection = d3.geo.orthographic()
+   .scale(50)
+   .translate([(width / 2) -350, (height / 2)-250])
+   .clipAngle(90)
+   .precision(0.3);
+
+// markers
+var city_color="#ffba00"
+var city_opacity=1
+var sphere_color="#0D0D0D"
+
+// create path variable, empty svg element and group container
+var path = d3.geo.path()
+   .projection(projection);
+var svg = d3.select("svg");
+var g = svg.append("g");
+
+// drawing dark grey spehere as landmass
+g.append("path")
+   .datum({type: "Sphere"})
+   .attr("class", "sphere")
+   .attr("d", path)
+   .attr("fill", sphere_color);
+
+g.append("text")
+   .html(city_label_1)
+   .attr('x', -120)
+   .attr('y', 145)
+   .style('fill', 'white')
+   .style("text-anchor", "beginning")
+   .style('font-family', '"Lucida Grande", sans-serif')
+   .style('font-size', '9px')
+   .attr("transform", "rotate(-90)" );
+
+g.append("text")
+   .html(city_label_2)
+   .attr('x', -120)
+   .attr('y', 160)
+   .style('fill', 'white')
+   .style("text-anchor", "beginning")
+   .style('font-family', '"Lucida Grande", sans-serif')
+   .style('font-size', '9px')
+   .attr("transform", "rotate(-90)" );
+
+g.append("text")
+   .html(city_label_3)
+   .attr('x', -120)
+   .attr('y', 175)
+   .style('fill', 'white')
+   .style("text-anchor", "beginning")
+   .style('font-family', '"Lucida Grande", sans-serif')
+   .style('font-size', '9px')
+   .attr("transform", "rotate(-90)" );
+
+// loading city locations from geoJSON
+d3.json(globe_data, function(error, data) {
+
+         // Handle errors getting and parsing the data
+         if (error) { return error; }
+
+         // setting the circle size (not radius!) according to the number of inhabitants per city
+         population_array = [];
+         for (i = 0; i < data.features.length; i++) {
+            population_array.push(data.features[i].properties.population);
+         }
+         max_population = population_array.sort(d3.descending)[0]
+         var rMin = 0;
+         var rMax = Math.sqrt(max_population / (peoplePerPixel * Math.PI));
+         rScale.domain([0, max_population]);
+         rScale.range([rMin, rMax]);
+
+         path.pointRadius(function(d) {
+            return d.properties ? rScale(d.properties.population) : 1;
+
+         });
+
+         // Drawing transparent circle markers for cities
+         g.selectAll("path.cities").data(data.features)
+            .enter().append("path")
+            .attr("class", "cities")
+            .attr("d", path)
+            .attr("fill", city_color)
+            .attr("fill-opacity", city_opacity);
+
+         // start spinning!
+         spinning_globe();
+});
+
+function spinning_globe(){
+   d3.timer(function() {
+
+      // get current time
+      var dt = Date.now() - time;
+
+      // get the new position from modified projection function
+      projection.rotate([rotate[0] + velocity[0] * dt, rotate[1] + velocity[1] * dt]);
+
+      // update cities position = redraw
+      svg.selectAll("path.cities").attr("d", path);
+   });
+
+}
+
+
+// hackish approach to get bl.ocks.org to display individual height
+d3.select(self.frameElement).style("height", height + "px");
+
+}
